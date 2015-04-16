d6d942c14f671a459b6e3ec32c59804b68d46be0
Luis Carbuccia <lcarbuccia@gmail.com>
2015-04-16 17:19:56 -0400

    Started adding front end with AngularJS

---
+++ WebSite/Scripts/app.js
@@ -0,0 +1,5 @@
+var myAngularBerlinClockApp = new angular.module("BerlinClockApp", []);
+
+myAngularBerlinClockApp.controller('SecondsController', ['$scope', function($scope){
+	$scope.on = true;
+}])
