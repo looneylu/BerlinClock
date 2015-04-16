69f9d34a95739a3db51046cbbb66589529ef9750
Luis Carbuccia <lcarbuccia@gmail.com>
2015-04-16 14:53:24 -0400

    Refactored to make less brute force

--- WebSite/Scripts/BerlinClock.js
+++ WebSite/Scripts/BerlinClock.js
@@ -6,71 +6,41 @@ var secondsTime = function(date){
 }
 
 var singleMinutesTime = function(date){
-	var minutesString = "OOOO";
-	if (date.getMinutes() % 5 == 1)
-		return "YOOO";
-	else if(date.getMinutes() % 5 == 2)
-		return "YYOO";
-	else if(date.getMinutes() % 5 == 3)
-		return "YYYO";
-	else if (date.getMinutes() % 5 == 4)
-		return "YYYY";
-	else 
-		return minutesString;
+	var minutes = date.getMinutes();
+	var numberOfSingleMinutes = minutes % 5;
+
+	return turnOnLamps(numberOfSingleMinutes, function(){ return 'Y'; });
 }
 
 var fifthMinutesTime = function(date){
-	var fifthMinutesString = "OOOOOOOOOOO";
-	if (date.getMinutes() / 5 == 1)
-		return "YOOOOOOOOOO";
-	else if (date.getMinutes() / 5 == 2)
-		return "YYOOOOOOOOO";
-	else if (date.getMinutes() / 5 == 3)
-		return "YYROOOOOOOO";
-	else if (date.getMinutes() / 5 == 4)
-		return "YYRYOOOOOOO";
-	else if (date.getMinutes() / 5 == 5)
-		return "YYRYYOOOOOO";
-	else if (date.getMinutes() / 5 == 6)
-		return "YYRYYROOOOO";
-	else if (date.getMinutes() / 5 == 7)
-		return "YYRYYRYOOOO";
-	else if (date.getMinutes() / 5 == 8)
-		return "YYRYYRYYOOO";
-	else if (date.getMinutes() / 5 == 9)
-		return "YYRYYRYYROO";
-	else if (date.getMinutes() / 5 == 10)
-		return "YYRYYRYYRYO";
-	else if (date.getMinutes() / 5 == 11)
-		return "YYRYYRYYRYY";
-	else
-		return fifthMinutesString;
+	var minutes = date.getMinutes();
+	var numberOfFives = minutes / 5;
+
+	return turnOnLamps(numberOfFives, function(currentElement){
+			return (currentElement % 3 == 0) ? 'R' : 'Y';
+	}, "OOOOOOOOOOO")
 }
 
 var singleHoursTime = function(date){
-	var singleHoursString = "OOOO";
-	if (date.getHours() % 5 == 1)
-		return "ROOO";
-	else if (date.getHours() % 5 == 2)
-		return "RROO";
-	else if (date.getHours() % 5 == 3)
-		return "RRRO";
-	else if (date.getHours() % 5 == 4)
-		return "RRRR";
-	else
-		return singleHoursString;
+	var hours = date.getHours();
+	var numberOfSingleHours = hours % 5;  
+
+	return turnOnLamps(numberOfSingleHours, function(){ return 'R'; });
+
 }
 
 var fifthHoursTime = function(date){
-	var fifthHoursString = "OOOO";
-	if (date.getHours() / 5 == 1)
-		return "ROOO";
-	else if (date.getHours() / 5 == 2)
-		return "RROO";
-	else if (date.getHours() / 5 == 3)
-		return "RRRO";
-	else if (date.getHours() / 5 == 4)
-		return "RRRR"; 
-	else
-		return fifthHoursString; 
+	var fifthHours = date.getHours();
+	var numberOfFifthHours = fifthHours / 5;
+
+	return turnOnLamps(numberOfFifthHours, function(){ return 'R'; });
+}
+
+var turnOnLamps = function(number, characterToUse, template){
+	return (template || "OOOO").split('').map(function(x, i){
+		if (i + 1 <= number){
+			return characterToUse(i+1);
+		} else
+			return 'O';
+	}).join("");
 }
