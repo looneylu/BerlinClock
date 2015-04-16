1f1951e4ff8a201c808a20a92aec620b93b6bc53
Luis Carbuccia <lcarbuccia@gmail.com>
2015-04-16 18:52:07 -0400

    Added ticking seconds to front end

--- WebSite/Scripts/app.js
+++ WebSite/Scripts/app.js
@@ -1,5 +1,11 @@
+
 var myAngularBerlinClockApp = new angular.module("BerlinClockApp", []);
 
-myAngularBerlinClockApp.controller('SecondsController', ['$scope', function($scope){
-	$scope.on = false;
+myAngularBerlinClockApp.controller('SecondsController', ['$scope', '$interval', function($scope, $interval){
+
+	$interval(function(){
+		secondsOn = secondsTime(new Date());
+		$scope.on = secondsOn;
+		}, 1000
+	);
 }])
