80df18e3711d56cd915359064cd416bc3f57e291
Luis Carbuccia <LuisCarbuccia@Luiss-MacBook-Pro.local>
2015-04-16 22:31:11 -0400

    Added TimeStamp

--- WebSite/Scripts/app.js
+++ WebSite/Scripts/app.js
@@ -4,8 +4,18 @@ var myAngularBerlinClockApp = new angular.module("BerlinClockApp", []);
 myAngularBerlinClockApp.controller('SecondsController', ['$scope', '$interval', function($scope, $interval){
 
 	$interval(function(){
-		secondsOn = secondsTime(new Date());
-		$scope.on = secondsOn;
+		time = new Date();
+		secondsOn = secondsTime(time);
+		$scope.secondsLamp = secondsOn;
+		fiveHourLamps = fifthHoursTime(time);
+		$scope.fiveHourLamps = fiveHourLamps;
+		singleHourLamps = singleHoursTime(time);
+		$scope.singleHourLamps = singleHourLamps; 
+		minutesOfFiveLamps = fifthMinutesTime(time);
+		$scope.fiveMinuteLamps = minutesOfFiveLamps;
+		minuteLamps = singleMinutesTime(time);
+		$scope.minuteLamps = minuteLamps;
+		$scope.timeString = time; 
 		}, 1000
 	);
 }])
