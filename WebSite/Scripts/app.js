8ff9ed46281d454cbdc6b4d75574251c6d701d0d
Luis Carbuccia <LuisCarbuccia@Luiss-MacBook-Pro.local>
2015-04-17 00:28:28 -0400

    Just need to finish tying front end

--- WebSite/Scripts/app.js
+++ WebSite/Scripts/app.js
@@ -1,21 +1,38 @@
 
 var myAngularBerlinClockApp = new angular.module("BerlinClockApp", []);
 
-myAngularBerlinClockApp.controller('SecondsController', ['$scope', '$interval', function($scope, $interval){
+/*var updateMinutes: function(minutes){
+	var minuteLamps = document.getElementById("minutes").children;
+	var yellow = "blue";
+	var red = "red"; 
+
+	for (var i = 0 ; i <= minuteLamps.length ; i++)
+		if(minutes[i] == 'Y')
+			minuteLamps[i].classList.add(yellow); 
+}*/
+
+myAngularBerlinClockApp.controller('timeController', ['$scope', '$interval', function($scope, $interval){
 
 	$interval(function(){
 		time = new Date();
 		secondsOn = secondsTime(time);
 		$scope.secondsLamp = secondsOn;
 		fiveHourLamps = fifthHoursTime(time);
-		$scope.fiveHourLamps = fiveHourLamps;
+		$scope.fiveHourLamps = fiveHourLamps.split("");
 		singleHourLamps = singleHoursTime(time);
-		$scope.singleHourLamps = singleHourLamps; 
+		$scope.singleHourLamps = singleHourLamps.split(""); 
 		minutesOfFiveLamps = fifthMinutesTime(time);
-		$scope.fiveMinuteLamps = minutesOfFiveLamps;
+		$scope.fiveMinuteLamps = minutesOfFiveLamps.split("");
 		minuteLamps = singleMinutesTime(time);
-		$scope.minuteLamps = minuteLamps;
+		$scope.minuteLamps = minuteLamps.split("");
 		$scope.timeString = time; 
+
+//		updateMinutes(minuteLamps.split("")); 
 		}, 1000
 	);
 }])
+
+
+
+
+
